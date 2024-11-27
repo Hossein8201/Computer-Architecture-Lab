6744075abbe32416c4a3ea58078b8326e24f6f48
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-11-27 11:16:08 +0330

    add Az7-MemoryTypes

--- Az7_MemoryTypes/VHDL code/3-DualPortRAM/DualPortRAM.vhd
+++ Az7_MemoryTypes/VHDL code/3-DualPortRAM/DualPortRAM.vhd
@@ -4,40 +4,51 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity DualPortRAM is
-    Port (
+    Port ( 
         clk : in STD_LOGIC;
         reset : in STD_LOGIC;
-        wr : in STD_LOGIC;
-        rd : in STD_LOGIC;
-        addr_wr : in STD_LOGIC_VECTOR(3 downto 0);
-        addr_rd : in STD_LOGIC_VECTOR(3 downto 0);
-        data_in : in STD_LOGIC_VECTOR(7 downto 0);
-        data_out : out STD_LOGIC_VECTOR(7 downto 0)
+        WR1 : in STD_LOGIC;
+        RD1 : in STD_LOGIC;
+        Addr1 : in STD_LOGIC_VECTOR(3 downto 0);
+        data_in1 : in STD_LOGIC_VECTOR(7 downto 0);
+        data_out1 : out STD_LOGIC_VECTOR(7 downto 0);
+     
+        WR2 : in STD_LOGIC;
+        RD2 : in STD_LOGIC;
+        Addr2 : in STD_LOGIC_VECTOR(3 downto 0);
+        data_in2 : in STD_LOGIC_VECTOR(7 downto 0);
+        data_out2 : out STD_LOGIC_VECTOR(7 downto 0)
     );
 end DualPortRAM;
 
 architecture Behavioral of DualPortRAM is
 
-    type memory_array is array (0 to 15) of STD_LOGIC_VECTOR(7 downto 0);
+    type memory_arr is array (0 to 15) of STD_LOGIC_VECTOR(7 downto 0);
 
-    signal memory : memory_array := (others => (others => '0'));
-    signal data_reg : STD_LOGIC_VECTOR(7 downto 0);
-
+    signal memory : memory_arr := (others => (others => '0'));
+    signal data1, data2 : STD_LOGIC_VECTOR(7 downto 0);
 begin
     process(clk, reset) begin
         if reset = '0' then
             memory <= (others => (others => '0'));
-            data_reg <= (others => '0');
+	    data1 <= (others => '0'); 
+	    data2 <= (others => '0');
         elsif (clk' event and clk = '1') then
-	    if wr = '1' then
-                memory(conv_integer(unsigned(addr_wr))) <= data_in;
+            if (WR1 = '1' and RD1 = '0') then
+                memory(conv_integer(unsigned(Addr1))) <= data_in1;
+            elsif (RD1 = '1' and WR1 = '0') then
+                data1 <= memory(conv_integer(unsigned(Addr1)));
             end if;
-	    if rd = '1' then
-                data_reg <= memory(conv_integer(unsigned(addr_rd)));
+
+            if (WR2 = '1' and RD2 = '0') then
+                memory(conv_integer(unsigned(Addr2))) <= data_in2;
+            elsif (RD2 = '1' and WR2 = '0') then
+                data2 <= memory(conv_integer(unsigned(Addr2)));
             end if;
         end if;
     end process;
-    data_out <= data_reg;
-
+    data_out1 <= data1; 
+    data_out2 <= data2;
+    
 end Behavioral;
 
--- Az7_MemoryTypes/VHDL code/3-DualPortRAM/DualPortRAM_tb.vhd
+++ Az7_MemoryTypes/VHDL code/3-DualPortRAM/DualPortRAM_tb.vhd
@@ -9,26 +9,36 @@ end DualPortRAM_tb;
 architecture Behavioral of DualPortRAM_tb is
 
     component DualPortRAM is
-        Port (
+        Port ( 
             clk : in STD_LOGIC;
             reset : in STD_LOGIC;
-            wr : in STD_LOGIC;
-            rd : in STD_LOGIC;
-            addr_wr : in STD_LOGIC_VECTOR(3 downto 0);
-            addr_rd : in STD_LOGIC_VECTOR(3 downto 0);
-            data_in : in STD_LOGIC_VECTOR(7 downto 0);
-            data_out : out STD_LOGIC_VECTOR(7 downto 0)
+            WR1 : in STD_LOGIC;
+            RD1 : in STD_LOGIC;
+            Addr1 : in STD_LOGIC_VECTOR(3 downto 0);
+            data_in1 : in STD_LOGIC_VECTOR(7 downto 0);
+            data_out1 : out STD_LOGIC_VECTOR(7 downto 0);
+            
+            WR2 : in STD_LOGIC;
+            RD2 : in STD_LOGIC;
+            Addr2 : in STD_LOGIC_VECTOR(3 downto 0);
+            data_in2 : in STD_LOGIC_VECTOR(7 downto 0);
+            data_out2 : out STD_LOGIC_VECTOR(7 downto 0)
         );
     end component;
 
     signal clk : STD_LOGIC := '0';
     signal reset : STD_LOGIC := '1';
-    signal wr : STD_LOGIC := '0';
-    signal rd : STD_LOGIC := '0';
-    signal addr_wr : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal addr_rd : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal data_in : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-    signal data_out : STD_LOGIC_VECTOR(7 downto 0);
+    signal WR1 : STD_LOGIC := '0';
+    signal RD1 : STD_LOGIC := '0';
+    signal Addr1 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    signal data_in1 : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+    signal data_out1 : STD_LOGIC_VECTOR(7 downto 0);
+    
+    signal WR2 : STD_LOGIC := '0';
+    signal RD2 : STD_LOGIC := '0';
+    signal Addr2 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    signal data_in2 : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+    signal data_out2 : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 
@@ -36,57 +46,59 @@ begin
         Port map (
             clk => clk,
             reset => reset,
-            wr => wr,
-            rd => rd,
-            addr_wr => addr_wr,
-            addr_rd => addr_rd,
-            data_in => data_in,
-            data_out => data_out
+            WR1 => WR1,
+            RD1 => RD1,
+            Addr1 => Addr1,
+            data_in1 => data_in1,
+            data_out1 => data_out1,
+            WR2 => WR2,
+            RD2 => RD2,
+            Addr2 => Addr2,
+            data_in2 => data_in2,
+            data_out2 => data_out2
         );
 
-    clk_process : process
-    begin
+    process begin
         clk <= '0';
         wait for 25 ns;
         clk <= '1';
         wait for 25 ns;
-    end process clk_process;
+    end process;
 
-    stim_proc: process
-    begin
+    process begin
         reset <= '0';
         wait for 50 ns;
         reset <= '1';
         wait for 50 ns;
 
-        addr_wr <= "0001";
-        data_in <= "10101010";
-        wr <= '1';
-        addr_rd <= "0000";
-        rd <= '1';
+        Addr1 <= "0001";
+        data_in1 <= "10101010";
+        WR1 <= '1';
+        Addr2 <= "0010";
+        data_in2 <= "11110000";
+        WR2 <= '1';
         wait for 50 ns;
-        wr <= '0';
-        rd <= '0';
-        wait for 50 ns;
-
-        addr_wr <= "0010";
-        data_in <= "11110000";
-        wr <= '1';
-        addr_rd <= "0001";
-        rd <= '1';
-        wait for 50 ns;
-        wr <= '0';
-        rd <= '0';
+        WR1 <= '0';
+        WR2 <= '0';
         wait for 50 ns;
 
-	addr_wr <= "0100";
-        data_in <= "10101111";
-        wr <= '1';
-        addr_rd <= "0010";
-        rd <= '1';
+        Addr1 <= "0001";
+        RD1 <= '1';
+        Addr2 <= "0011";
+        data_in2 <= "01010101";
+        WR2 <= '1';
         wait for 50 ns;
-        wr <= '0';
-        rd <= '0';
+        RD1 <= '0';
+        WR2 <= '0';
+        wait for 50 ns;
+
+        Addr1 <= "0010";
+        RD1 <= '1';
+        Addr2 <= "0011";
+        RD2 <= '1';
+        wait for 50 ns;
+        RD1 <= '0';
+        RD2 <= '0';
         wait for 50 ns;
 
         wait;
